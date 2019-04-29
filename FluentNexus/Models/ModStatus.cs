4fd282be6d9068812f355a7c34e4095e40625c34
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-04-29 00:15:22 -0400

    add missing mod statuses

--- FluentNexus/Models/ModStatus.cs
+++ FluentNexus/Models/ModStatus.cs
@@ -15,10 +15,21 @@ namespace Pathoschild.FluentNexus.Models
         /// <summary>The mod is published and visible.</summary>
         Published,
 
+        /// <summary>The mod should be published once the game goes live.</summary>
+        [EnumMember(Value = "publish_with_game")]
+        PublishWithGame,
+
         /// <summary>The mod has been hidden by the author.</summary>
         Hidden,
 
-        /// <summary>The mod has been deleted.</summary>
-        Wastebinned
+        /// <summary>The mod is hidden while it undergoes moderator review.</summary>
+        [EnumMember(Value = "under_moderation")]
+        UnderModeration,
+
+        /// <summary>The mod has been recoverably deleted.</summary>
+        Wastebinned,
+
+        /// <summary>The mod has been permanently removed.</summary>
+        Removed
     }
 }
